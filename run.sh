d3a2d55a9935bb66f44b23fbae341f07083cc7a5
pchynoweth <24738364+pchynoweth@users.noreply.github.com>
2020-07-03 22:02:15 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -ex
 
-SHA=$(git rev-parse HEAD)
+SHA=$(git rev-parse origin/$BRANCH_NAME)
 
 curl -X POST  -H "Accept: application/vnd.github.v3+json" \
 https://$GITHUB_SECRET:x-oauth-basic@api.github.com/repos/pchynoweth/jenkins-test/statuses/$SHA \
